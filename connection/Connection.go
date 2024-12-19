28cc79b928c4ce29bd05031bc3a2ab90e7e65112
Valerio Vinciarelli <valerio.vinciarelli@epos-eric.eu>
2024-12-19 11:07:27 +0100

    Update using gorm

--- connection/Connection.go
+++ connection/Connection.go
@@ -2,28 +2,91 @@ package connection
 
 import (
 	"fmt"
-	"os"
-
 	"github.com/epos-eu/converter-service/orms"
-	"github.com/go-pg/pg/v10"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+	"log"
+	"os"
+	"regexp"
+	"strings"
 )
 
-func Connect() (*pg.DB, error) {
-	conn := ""
-
-	if val, res := os.LookupEnv("POSTGRESQL_CONNECTION_STRING"); res == true {
-		conn = val
-	} else {
+// get a db to execute queries
+func Connect() (*gorm.DB, error) {
+	dsn, ok := os.LookupEnv("POSTGRESQL_CONNECTION_STRING")
+	log.Println("POSTGRESQL_CONNECTION_STRING:", dsn)
+	if !ok {
 		return nil, fmt.Errorf("POSTGRESQL_CONNECTION_STRING is not set")
 	}
 
-	opt, err := pg.ParseURL(conn)
-	if err != nil {
-		return nil, err
-	}
-	db := pg.Connect(opt)
+	// Remove the "jdbc:" prefix if it exists
+	dsn = strings.Replace(dsn, "jdbc:", "", 1)
 
-	return db, nil
+	log.Println("Cleaned DSN (jdbc prefix removed):", dsn)
+
+	// Remove unsupported parameters like targetServerType and loadBalanceHosts
+	re := regexp.MustCompile(`(&?(targetServerType|loadBalanceHosts)=[^&]+)`)
+	dsn = re.ReplaceAllString(dsn, "")
+
+	log.Println("Cleaned DSN (unsupported parameters removed):", dsn)
+
+	// Clean up trailing "?" or "&"
+	dsn = regexp.MustCompile(`[?&]$`).ReplaceAllString(dsn, "")
+
+	log.Println("Cleaned DSN (multi-host supported):", dsn)
+
+	// Parse hosts and connection parameters correctly
+	hostStart := strings.Index(dsn, "//")
+	if hostStart == -1 {
+		return nil, fmt.Errorf("invalid connection string format: missing '//'")
+	}
+
+	// Extract everything after `//` (hosts and parameters)
+	hostsAndParams := dsn[hostStart+2:]
+	splitIndex := strings.Index(hostsAndParams, "/")
+	if splitIndex == -1 {
+		return nil, fmt.Errorf("invalid connection string format: missing '/' after hosts")
+	}
+
+	hosts := hostsAndParams[:splitIndex]
+	params := hostsAndParams[splitIndex+1:]
+
+	hostList := strings.Split(hosts, ",")
+
+	log.Printf("Parsed Hosts: %v", hostList)
+	log.Printf("Connection Parameters: %s", params)
+
+	var db *gorm.DB
+	var err error
+	var lastError error
+
+	// Attempt to connect to each host
+	for _, host := range hostList {
+		currentDSN := fmt.Sprintf("postgresql://%s/%s", host, params)
+		log.Printf("Attempting to connect to: %s", host)
+		db, err = gorm.Open(postgres.New(postgres.Config{
+			DriverName: "pgx",
+			DSN:        currentDSN,
+		}), &gorm.Config{
+			NamingStrategy: schema.NamingStrategy{
+				TablePrefix:   "",   // table name prefix
+				SingularTable: true, // use singular table names
+			},
+		})
+		if err == nil {
+			log.Printf("Successfully connected to: %s", host)
+			return db, nil
+		}
+		log.Printf("Failed to connect to: %s, error: %v", host, err)
+		lastError = err // Keep track of the last error
+	}
+
+	// Return the last error if no connection is successful
+	if lastError != nil {
+		log.Printf("Returning the last error encountered: %v", lastError)
+	}
+	return nil, fmt.Errorf("all hosts are unreachable: %w", lastError)
 }
 
 //func GetSoftwareSourceCodes() []orms.SoftwareSourceCode {
@@ -67,10 +130,9 @@ func GetPlugins() ([]orms.Plugin, error) {
 	if err != nil {
 		return nil, err
 	}
-	defer db.Close()
 	// Select all users.
 	var listOfPlugins []orms.Plugin
-	err = db.Model(&listOfPlugins).Select()
+	err = db.Model(&listOfPlugins).Find(&listOfPlugins).Error
 	if err != nil {
 		return nil, err
 	}
@@ -82,10 +144,9 @@ func GetPluginRelations() ([]orms.PluginRelations, error) {
 	if err != nil {
 		return nil, err
 	}
-	defer db.Close()
 	// Select all users.
 	var listOfPluginRelations []orms.PluginRelations
-	err = db.Model(&listOfPluginRelations).Select()
+	err = db.Model(&listOfPluginRelations).Find(&listOfPluginRelations).Error
 	if err != nil {
 		panic(err)
 	}
@@ -98,8 +159,7 @@ func GetPluginRelationsById(id string) (orms.PluginRelations, error) {
 	if err != nil {
 		return plugin, err
 	}
-	defer db.Close()
-	err = db.Model(&plugin).Where("id = ?", id).Select()
+	err = db.Model(&plugin).Where("id = ?", id).First(&plugin).Error
 	if err != nil {
 		return plugin, err
 	}
@@ -111,18 +171,17 @@ func GetPluginRelationsByOperationId(operationId string) ([]orms.PluginRelations
 	if err != nil {
 		return nil, err
 	}
-	defer db.Close()
 
 	// Get the operation by id
 	var operation orms.Operation
-	err = db.Model(&operation).Where("uid = ?", operationId).Select()
+	err = db.Model(&operation).Where("uid = ?", operationId).First(&operation).Error
 	if err != nil {
 		return nil, err
 	}
 
 	// Get the plugin relations by operationInstanceId
 	var listOfPluginRelations []orms.PluginRelations
-	err = db.Model(&listOfPluginRelations).Where("relation_id = ?", operation.Instance_id).Select()
+	err = db.Model(&listOfPluginRelations).Where("relation_id = ?", operation.InstanceID).Find(&listOfPluginRelations).Error
 	if err != nil {
 		return nil, err
 	}
@@ -138,8 +197,7 @@ func GetPluginById(pluginId string) (orms.Plugin, error) {
 	if err != nil {
 		return plugin, err
 	}
-	defer db.Close()
-	err = db.Model(&plugin).Where("id = ?", pluginId).Select()
+	err = db.Model(&plugin).Where("id = ?", pluginId).First(&plugin).Error
 	if err != nil {
 		return plugin, err
 	}
@@ -153,11 +211,10 @@ func EnablePlugin(id string, enable bool) error {
 	if err != nil {
 		return err
 	}
-	defer db.Close()
-	_, err = db.Model(plugin).
-		Set("enabled = ?", enable).
+	err = db.Model(plugin).
+		Update("enabled", enable).
 		Where("id = ?", id).
-		Update()
+		Error
 	if err != nil {
 		return err
 	}
